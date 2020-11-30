1d936fb8de63eb82cfb183601f40ed0d74b3ca61
Romildo Feliciano <romildofa22@gmail.com>
2020-11-30 14:59:41 -0300

    app_final_01

--- app/src/main/java/com/example/trabalhoUnifor/ErrorActiviy.java
+++ app/src/main/java/com/example/trabalhoUnifor/ErrorActiviy.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ErrorActiviy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_error_activiy);
 
         Button btnBack = (Button) findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoadingActivity.class);
+                Intent intent = new Intent(ErrorActiviy.this, LoadingActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/trabalhoUnifor/LoadingActivity.java
+++ app/src/main/java/com/example/trabalhoUnifor/LoadingActivity.java
@@ -3,12 +3,10 @@ package com.example.trabalhoUnifor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Loading_Activity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity {
 
 
 
@@ -26,7 +24,7 @@ public class Loading_Activity extends AppCompatActivity {
     }
 
     private void mostrarLogin() {
-        Intent intent = new Intent(Loading_Activity.this, MainActivity.class);
+        Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
