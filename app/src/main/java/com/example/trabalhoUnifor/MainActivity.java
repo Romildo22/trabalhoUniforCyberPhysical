88999a9f2621e7657fc61f34e4645f124d9d2e61
Romildo Feliciano <romildofa22@gmail.com>
2020-12-11 18:23:49 -0300

    ajustes

--- app/src/main/java/com/example/trabalhoUnifor/MainActivity.java
+++ app/src/main/java/com/example/trabalhoUnifor/MainActivity.java
@@ -7,7 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+     private FirebaseDatabase firebaseDatabase;
+     private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        inicializarFirebase();
+    }
+
+    private void inicializarFirebase(){
+        FirebaseApp.initializeApp(MainActivity.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
 }
